f4d50799aeaf86def058a3c65ee017a84a893ac5
Shraddha2424 <shraddha.ajabe@waiin.com>
2025-06-26 12:40:33 +0530

    changes done

--- src/app/modal/project-modal/project-modal.ts
+++ src/app/modal/project-modal/project-modal.ts
@@ -2,17 +2,17 @@ import { CommonModule, NgFor } from '@angular/common';
 import { Component, EventEmitter, Input, NgModule, Output, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { ConfirmationService } from 'primeng/api';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
+ 
+ 
 
 @Component({
   selector: 'app-project-modal',
   imports: [NgFor, CommonModule,
-    FormsModule, ButtonModule, ConfirmDialogModule
+    FormsModule, ButtonModule 
   ],
   templateUrl: './project-modal.html',
   styleUrl: './project-modal.css',
-  providers: [ConfirmationService]
+  providers: [ ]
 })
 export class ProjectModal {
   @Input() isVisible: boolean = false;
@@ -45,7 +45,7 @@ export class ProjectModal {
     description: ''
   };
   
-  constructor(private confirmationService: ConfirmationService) { }
+  constructor () { }
 
   ngOnInit(): void {
   }
@@ -79,29 +79,5 @@ export class ProjectModal {
       description: ''
     };
   }
-
-  confirmCancel() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to cancel?',
-      header: 'Cancel Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.closeModal();
-      },
-      reject: () => {
-        // Handle rejection logic if needed
-      },
-    });
-  }
-
-  confirmSave() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to save the changes?',
-      header: 'Save Confirmation',
-      icon: 'pi pi-check-circle',
-      accept: () => {
-        this.onSubmit();
-      }
-    });
-  }
+ 
 }
